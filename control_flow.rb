d4ce3f231a63a90200ad95359e303c49c14f5c07
Eva Reitzel <evareitzel@gmail.com>
2022-11-10 11:06:58 -0500

    commit tree 71354f14aa0255a7b70606400fee39079a19f369

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,47 @@
 def admin_login(username, password)
   # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+  "Access denied"
+  end
 end
 
-def hows_the_weather(temperature)
+def hows_the_weather(temp)
   # your code here
+  resp =  if temp < 40
+                "brisk"
+              elsif temp >= 40 && temp <= 65
+                'a little chilly'
+              elsif temp > 85
+                'too dang hot'
+              else
+                'perfect'
+              end
+  "It's #{resp} out there!"
 end
 
 def fizzbuzz(num)
   # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  else puts "Invalid operation!"
+  # unless? != puts "Invalid operation!" return nil
 end
-
+end
